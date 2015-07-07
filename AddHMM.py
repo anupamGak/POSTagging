ac222a333cf3100e95df31a016b5b0b49231befb
anupamGak <anupamGak07@gmail.com>
2015-07-07 18:45:26 +0530

    Sml changes

--- AddHMM.py
+++ AddHMM.py
@@ -1,45 +1,45 @@
 import re
 from collections import Counter
 
-class HMM():
+class OuterHMM():
 	def __init__(self):
-		reTag = re.compile(r" (.+)$", re.MULTILINE)
-		reSym = re.compile(r"^(.+) ", re.MULTILINE)
-		with open('datasets/segment.txt', 'r') as trfile:
+		reEle = re.compile(r"\/\/([A-Z]{2})")
+		reSym = re.compile(r"^(.+)//", re.MULTILINE)
+		with open('datasets/train_data2.txt', 'r') as trfile:
 			self.data = trfile.read()
-		self.taglist = re.findall(reTag, self.data)
+		self.elelist = re.findall(reEle, self.data)
 		self.symlist = re.findall(reSym, self.data)
 
-		self.tagcount = dict(Counter(self.taglist))
+		self.elecount = dict(Counter(self.elelist))
 		self.symcount = dict(Counter(self.symlist))
 
-		self.tags = self.tagcount.keys()
+		self.eles = self.elecount.keys()
 		self.syms = self.symcount.keys()
 
 #		self.build_transition()
 		self.build_emission()
 
 	def build_transition(self):
-		tg_seq = " ".join(self.taglist)
+		ele_seq = " ".join(self.elelist)
 
 		self.tr_matrix = {}
-		for i in self.tags:
+		for i in self.eles:
 			self.tr_matrix[i] = {}
-			for j in self.tags:
-				N = tg_seq.count(i+" "+j+" ")
-				D = tg_seq.count(i)
+			for j in self.eles:
+				N = ele_seq.count(i+" "+j+" ")
+				D = ele_seq.count(i)
 				self.tr_matrix[i][j] = N/float(D)
 		print self.tr_matrix
 
 	def build_emission(self):
 		self.em_matrix = {}
-		for j in self.tags:
+		for j in self.eles:
 			self.em_matrix[j] = {}
 			for k in self.syms:
-				N = self.data.count(k+" "+j) + 1
-				D = self.data.count(j) + len(self.syms)
+				N = self.data.count(k+"//"+j) #+ 1
+				D = self.data.count(j) #+ len(self.syms)
 				self.em_matrix[j][k] = N/float(D)
 		print self.em_matrix
 
 
-hmm = HMM()
+hmm = OuterHMM()
